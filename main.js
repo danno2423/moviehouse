2893703b734a02924d434d0f504c57781a7dbcb0
danno2423 <danno2423@gmail.com>
2015-09-24 13:48:31 -0400

    fifth commit

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-$(document).ready(
+$(document).ready(function() {
 
-	function(){
-		$('#About').click(
-
-			
-			)
-
-	}
-
-)
+		$('a[href^="#"]').on('click', function(event) {
+	    var target = $($(this).attr('href'));
+	    if( target.length ) {
+	        event.preventDefault();
+	        $('html, body').animate({
+	            scrollTop: target.offset().top
+	        }, 1000);
+	    }
+	});
+});
